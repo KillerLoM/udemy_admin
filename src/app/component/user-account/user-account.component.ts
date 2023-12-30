9269141dca8bf460775ab7427cb9f8f98340849a
KillerLoM <nguyenminhtam01632.fetel@gmail.com>
2023-12-30 23:55:21 +0700

    update

--- src/app/component/user-account/user-account.component.ts
+++ src/app/component/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/Model/user';
 import { ShareService } from 'src/app/Service/share.service';
 
@@ -9,11 +10,23 @@ import { ShareService } from 'src/app/Service/share.service';
 })
 export class UserAccountComponent {
     user: User| null = null;
+    idUser: number | null = null
+    myForm: FormGroup;
     constructor(
       @Inject(ShareService) private shareService: ShareService,
+      private fb: FormBuilder,
+      private _formBuilder: FormBuilder
     ){
       this.user = this.shareService.getUser();
-      console.log(this.user?.userDTO.fullname)
+      console.log(this.user?.userDTO.fullname);
+      if(this.user?.userDTO.id)
+      this.idUser = this.user?.userDTO.id;
+      this.myForm = this.fb.group({
+        telephoneControl: new FormControl('555-555-1234')
+      });
     }
-
+    userFormGroup = this._formBuilder.group({
+      idUser: ['', Validators.required],
+      fullname: ['', Validators.required],
+    });
 }
